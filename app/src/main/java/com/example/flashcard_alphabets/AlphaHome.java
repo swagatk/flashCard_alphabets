e0c1af639b02174b852396478d2f80f44ce78aa4
Swagat Kumar <swagat.kumar@gmail.com>
2022-10-08 00:27:18 +0100

    - Added sound to the alphabet home page.

--- app/src/main/java/com/example/flashcard_alphabets/AlphaHome.java
+++ app/src/main/java/com/example/flashcard_alphabets/AlphaHome.java
@@ -22,6 +22,9 @@ public class AlphaHome extends AppCompatActivity {
             "J, j", "K, k",
             "L, l", "M, m", "N, n", "O, o", "P, p", "Q, q", "R, r", "S, s", "T, t", "U, u", "V, v",
             "W, w", "X, x", "Y, y", "Z, z"};
+//    private String[] speakText = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+//               "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
